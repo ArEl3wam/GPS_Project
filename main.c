0bd0ec00eecff2f65b7c126d8a6a675ca8adb338
Ahmed <arelawam@gmail.com>
2021-06-05 08:29:57 +0200

    Adding Delay Functions

--- main.c
+++ main.c
@@ -73,3 +73,19 @@ void dist_to_display(uint16_t dist){
 void turn_LED_100(uint16_t dist){
 
 }
+
+void delay_milli(int n){
+int i,j;
+for(i=0;i<n;i++){
+for(j=0;j<3180;j++)
+{}
+}
+}
+ 
+void delay_micro(int n){
+int i,j;
+for(i=0;i<n;i++){
+for(j=0;j<3;j++)
+{}
+}
+}
